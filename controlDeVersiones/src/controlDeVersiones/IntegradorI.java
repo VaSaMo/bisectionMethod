e9c97cbb97256a34010f8facfe1f5a09deb2edb9
Vanessa Sanchez Morales <vasamo345@gmail.com>
2023-08-16 16:52:11 -0500

    changes made

--- controlDeVersiones/src/controlDeVersiones/IntegradorI.java
+++ controlDeVersiones/src/controlDeVersiones/IntegradorI.java
@@ -24,7 +24,7 @@ public class IntegradorI {
         }
         return pow;//Returns the final value of the "base" multiplied by "exponent" times.
     }
-<<<<<<< HEAD
+
     /*public static int factorial(int n){//Factorial function: based on a input number, multiplies said n by the smaller whole numbers.
         int factorial=1;    
 	}*/
@@ -38,7 +38,7 @@ public class IntegradorI {
     
     public static int factorial(int n){//Factorial function: based on a input number, multiplies said n by the smaller
         int factorial=1;               //whole numbers.
->>>>>>> origin/feature1
+
         for(int i=1;i<=n;i++){
             factorial*=i;
         }
